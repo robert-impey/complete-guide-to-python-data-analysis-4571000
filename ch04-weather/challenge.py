c87ec831b14992c97070a90ca6948a7221eeee2e
Robert Impey <robert-impey@users.noreply.github.com>
2026-01-04 00:52:49 +0000

    Ch4: Start of challenge

---
+++ ch04-weather/challenge.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def anomaly(allyears):
+    # Your code goes here.
+
+    return np.array([], dtype=np.float64)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
